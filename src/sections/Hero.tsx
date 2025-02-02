758291878a3cdcb4badb83a2df273fc1c8533f39
Shobhit010 <137671084+Shobhit010@users.noreply.github.com>
2025-02-02 23:15:34 +0530

    Update Hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -78,7 +78,7 @@ export const HeroSection = () => {
         <div className='max-w-lg mx-auto'>
           <h1 className='font-serif text-3xl md:text-5xl  text-center mt-8 tracking-wide'>Building   Exceptional User Experiences</h1>
           <p className='mt-4 text-center text-white/60 md:text-lg'>
-            I specialize in transforming designs into functional, high-performing web applications. Let's discuss your next project.
+            I specialize in transforming designs into functional, high-performing web applications. Let&apos;s discuss your next project.
           </p>
         </div>
         
@@ -89,7 +89,7 @@ export const HeroSection = () => {
           </button>
           <button className='inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl'>
             <span>👋</span>
-            <span className='font-semibold'>Let's connect</span>
+            <span className='font-semibold'>Let&apos;s connect</span>
           </button>
         </div>
       </div>
